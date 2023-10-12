0f8cf39686ab4d9b007c1399dcea12ef0ba64a24
eamonnmg <mcgonigle.eamonn@gmail.com>
2023-10-12 17:12:42 +0100

    separate refresh behavior to usePreviewBridge.ts and put live preview content stuff in useLivePreview.ts

--- packages/next/src/lib/usePreviewBridge.ts
+++ packages/next/src/lib/usePreviewBridge.ts
@@ -1,9 +1,8 @@
+'use client';
 import { useEffect } from 'react';
-import { AppRouterInstance } from 'next/dist/shared/lib/app-router-context.shared-runtime';
-export default function usePreviewBridge(
-    draftMode: boolean,
-    router: AppRouterInstance
-) {
+import { useRouter as usePagesRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
+export function usePreviewBridge(draftMode: boolean) {
     // only run client side and in draft mode
     if (typeof window === 'undefined' || !draftMode) {
         return;
@@ -15,15 +14,28 @@ export default function usePreviewBridge(
         }
     }
 
+    let refresh: () => void;
+
+    // use correct router to refresh depening on if next is
+    // using app router or pages router
+    try {
+        const router = usePagesRouter();
+        refresh = () =>
+            router.replace(router.asPath, undefined, { scroll: false });
+    } catch {
+        const router = useRouter();
+        refresh = () => router.refresh();
+    }
+
+    function refreshPreview(event: MessageEvent) {
+        if (event.data.message !== 'contento-refresh-preview') {
+            return;
+        }
+        refresh();
+    }
+
     emitLoadedEvent();
     useEffect(() => {
-        function refreshPreview(event: MessageEvent) {
-            if (event.data.message !== 'contento-refresh-preview') {
-                return;
-            }
-            router.refresh();
-        }
-
         window.addEventListener('message', refreshPreview);
         // remove event listeners on cleanup
         return () => {
