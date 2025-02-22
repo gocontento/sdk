08ce3f975dd9caf6c0d7917b7574b8ec3b851232
Josh Angell <josh@contento.io>
2025-02-22 15:35:37 +0000

    update nx and other deps

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -6,6 +6,7 @@ import dts from 'vite-plugin-dts';
 import * as path from 'path';
 
 export default defineConfig({
+    root: __dirname,
     cacheDir: '../node_modules/.vite/client',
 
     plugins: [
@@ -26,6 +27,9 @@ export default defineConfig({
     // Configuration for building your library.
     // See: https://vitejs.dev/guide/build.html#library-mode
     build: {
+        outDir: '../../dist/client',
+        reportCompressedSize: true,
+        commonjsOptions: { transformMixedEsModules: true },
         sourcemap: true,
         minify: false,
         lib: {
@@ -44,6 +48,11 @@ export default defineConfig({
     },
 
     test: {
+        reporters: ['default'],
+        coverage: {
+            reportsDirectory: '../coverage/client',
+            provider: 'v8',
+        },
         globals: true,
         cache: {
             dir: '../node_modules/.vitest',
