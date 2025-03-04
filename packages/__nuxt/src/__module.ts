2f978ba5d0952600745d22d9e2e9f19973ca3c40
Josh Angell <josh@contento.io>
2025-03-04 10:48:27 +0000

    mothball current nuxt

---
+++ packages/__nuxt/src/__module.ts
@@ -0,0 +1,14 @@
+export interface ModuleOptions {
+    apiKey: string;
+    siteId: string;
+    previewSecret: string;
+}
+
+// contentoApiUrl: process.env.NUXT_CONTENTO_API_URL,
+//     contentoApiKey: process.env.NUXT_CONTENTO_API_KEY,
+//     contentoSiteId: process.env.NUXT_CONTENTO_SITE_ID,
+//     siteMainNavId: process.env.NUXT_SITE_MAIN_NAV_ID,
+//     siteFooterNavId: process.env.NUXT_SITE_FOOTER_NAV_ID,
+//     public: {
+//     contentoPreviewSecret: process.env.NUXT_PUBLIC_CONTENTO_PREVIEW_SECRET,
+// },
